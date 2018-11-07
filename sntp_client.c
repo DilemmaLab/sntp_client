efcc6cd2638f17ef87d647114f63033cd29c3e55
DilemmaLab <10974541+DilemmaLab@users.noreply.github.com>
2018-11-08 01:19:40 +0300

    Update sntp_client.c

--- sntp_client.c
+++ sntp_client.c
@@ -12,30 +12,32 @@
   }
   ntpdate() {  
 	char  *hostname="91.226.136.136";
-  	int  portno=123;
-  	//NTP- порт 123  int  maxlen=1024;
-  	// размер буфера int  i;
-     	unsigned  /*  Построение сообщения длиной 48 байт, в котором все байты = 0, кроме  версии протокола 100, т.е. 4 */  
-	char  msg[48]={100,0,0,0,0,0,0,0,0};
-	//  отправляемый пакет unsigned  long  buf[ maxlen];
-  	//  буфер приема struct protoent *proto;
+  	int  portno=123; //NTP- порт 123  
+	int  maxlen=1024; // размер буфера 
+	int  i;
+     	/*  Построение сообщения длиной 48 байт, в котором все байты = 0, кроме  версии протокола 100, т.е. 4 */  
+	unsigned char  msg[48]={100,0,0,0,0,0,0,0,0}; //  отправляемый пакет 
+	unsigned  long  buf[ maxlen]; //  буфер приема 
+	struct protoent *proto;
   	struct sockaddr_in server_addr;
-  	int  s;
-   	//  соккет int    tclock1,  tclock2=0;
-	//  время на компьютере int  tmit;
-  	//  время сервера proto=getprotobyname("udp");
+  	int  s; //  соккет 
+	int    tclock1,  tclock2=0; //  время на компьютере 
+	int  tmit; //  время сервера 
+	proto=getprotobyname("udp");
   	s=socket(PF_INET, SOCK_DGRAM, proto- >p_proto);
   	memset( &server_addr, 0, sizeof( server_addr ));
   	server_addr.sin_family=AF_INET;
   	server_addr.sin_addr.s_addr = inet_addr(hostname);
   	server_addr.sin_port=htons(portno);
-  	tclock1 =clock();
-  	//Время на компьютере до отправки сообщения printf("Clock before sending request = %d\ n",tclock1);
-  	//  Отправка данных i=sendto(s,msg,sizeof(msg),0,(struct sockaddr  *)&server_addr,sizeof(server_addr));
-  	tclock2=clock();
- 	//Время на компьютере после отправки сообщения printf("Clock after sending request = %d\ n",tclock2);
+  	tclock1 =clock(); //Время на компьютере до отправки сообщения 
+	printf("Clock before sending request = %d\ n",tclock1); 
+	//  Отправка данных 
+	i=sendto(s,msg,sizeof(msg),0,(struct sockaddr  *)&server_addr,sizeof(server_addr));
+  	tclock2=clock(); //Время на компьютере после отправки сообщения 
+	printf("Clock after sending request = %d\ n",tclock2);
   	printf("Program spent %d ms on request (RTT)\ n",tclock2- tclock1);
-  	//  получение ответных данных i=recv( s,buf,   sizeof( buf),0);
+  	//  получение ответных данных 
+	i=recv( s,buf,   sizeof( buf),0);
   	printf("Socket descriptor %d\ n",s);
   	printf("recvfr: %d\ n",i);
   	perror ("recvfr");
@@ -55,7 +57,8 @@
   	updtm=ntohl((time_t)buf[7]);
   	updtm- = 2208988800U;
   	printf("Transmit Timestamp: %s",(char*)ctime(&updtm));
-  	// Первая строка буфера приема  - интерпретация при помощи побитовых  операций int val;
+  	// Первая строка буфера приема  - интерпретация при помощи побитовых  операций 
+	int val;
   	val=( ( buf[0] ) >> 30);
   	switch(val)  {  case 0: printf("No Warning LI=%d\ n", val);
  	break;
@@ -104,7 +107,9 @@
   float delay;
   delay=ntohl((float)buf[1]);
   printf("Delay = %f ms\ n",delay);
-  /*  Конвертация времени  NTP- сервера в стандартное время  Unix/ Linux- системы  - для этого необходимо вычесть из полученного времени число  секунд между  0000  UT   1  Января 1900  ( точка отсчета времени  NTP- сервера ) и  0000  UT   1  Января  1970  ( точка отсчета времени  Unix/ Linux- системы ) и  применить функцию  ctime(..)  */  
+  /*  Конвертация времени  NTP- сервера в стандартное время  Unix/ Linux- системы  - для этого необходимо 
+  вычесть из полученного времени число  секунд между  0000  UT   1  Января 1900  ( точка отсчета времени  NTP- сервера ) 
+  и  0000  UT   1  Января  1970  ( точка отсчета времени  Unix/ Linux- системы ) и  применить функцию  ctime(..)  */  
   tmit- = 2208988800U;
   printf("Server Time: %s",(char*)ctime(&tmit));
   i=time(0);
